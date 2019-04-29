9ca1fbaf32aab4a2665257e9292f368e7d16955d
U1710070 <bek.98166@gmail.com>
2019-04-29 12:29:23 +0500

    SSS

---
+++ resources/views/others/about.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.master')
+
+@section('content')
+    <div class="row">
+        <div class="col-md-12">
+            <p class="quote">The beautiful Laravel</p>
+        </div>
+    </div>
+    <p>This web-site was created by Davron Islamov (U1710261).</p>
+@endsection
